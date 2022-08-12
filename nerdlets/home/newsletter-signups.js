4dd951cb3d9ce5a9a98671431df31d5504046f35
fil7er <fil7er@gmail.com>
2022-08-12 09:18:54 -0400

    Update newsletter-signups.js

--- nerdlets/home/newsletter-signups.js
+++ nerdlets/home/newsletter-signups.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import {
+    Button,
     HeadingText,
     LineChart,
     NrqlQuery,
     PlatformStateContext,
+    Stack,
+    StackItem,
+    navigation,
 } from 'nr1';
 
 const ACCOUNT_ID = 'Removed'
+const ENTITY_GUID = 'Removed'
 
 export default class NewsletterSignups extends React.Component {
+    openAPMEntity() {
+        navigation.openStackedEntity(ENTITY_GUID)
+    }
+
     render() {
         return <div>
-            <HeadingText className="chartHeader">
-                Newsletter subscriptions per version
-            </HeadingText>
+            <Stack fullWidth>
+                <StackItem grow={true}>
+                    <HeadingText className="chartHeader">
+                        Newsletter subscriptions per version
+                    </HeadingText>
+                </StackItem>
+                <StackItem>
+                    <Button onClick={this.openAPMEntity}>
+                        App performance
+                    </Button>
+                </StackItem>
+            </Stack>
             <PlatformStateContext.Consumer>
                 {
                     (platformState) => {
